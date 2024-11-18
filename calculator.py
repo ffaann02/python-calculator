3bcdf9e648324e239b861129cbf5fa061d360550
Rudfaan Maimahad <84792604+ffaann02@users.noreply.github.com>
2024-11-18 06:42:21 +0000

    fix: calculator bugs

--- calculator.py
+++ calculator.py
@@ -2,25 +2,44 @@ class Calculator:
     def add(self, a, b):
         return a + b
 
+    # bugs: switch from: b - a => a - b
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
+    # bugs: remove extra loop, handle minus number
     def multiply(self, a, b):
+        positive_a = a
+        positive_b = b
+        if a<0:
+            positive_a = -a
+        if b<0:
+            positive_b = -b
         result = 0
-        for i in range(b+1):
-            result = self.add(result, a)
+        for _ in range(positive_b):
+            result = self.add(result, positive_a)
+        if a<0 and b>0:
+            result = -result
+        elif b<0 and a>0:
+            result = -result
+        
         return result
 
+    # bugs: handle with zero divider, replace > with => in while condition
     def divide(self, a, b):
+        if b==0:
+            raise ValueError("can't divide by zero")
         result = 0
-        while a > b:
+        while a >= b:
             a = self.subtract(a, b)
             result += 1
         return result
     
+    # bugs: handle mod zero ,change condition in while loop to opposite
     def modulo(self, a, b):
-        while a <= b:
-            a = a-b
+        if b==0:
+            raise ValueError("can't mod by zero")
+        while a >= b:
+            a = self.subtract(a, b)
         return a
 
 # Example usage:
